6dc12854d44d1874b24393484176d5290a7c14a2
Udjin <j-zen@yandex.ru>
2021-11-04 13:10:47 +0300

    selenide tests

---
+++ src/test/java/dad/DragAndDropTest.java
@@ -0,0 +1,47 @@
+package dad;
+
+import com.codeborne.selenide.*;
+import org.junit.jupiter.api.*;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.awt.event.MouseEvent;
+
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selenide.*;
+
+public class DragAndDropTest {
+
+    @BeforeAll
+    public static void setUpBeforeAll() {
+        Configuration.headless = false;
+        Configuration.startMaximized = true;
+    }
+
+    private final SelenideElement columnA = $("[id=column-a]");
+    private final SelenideElement columnB = $("[id=column-b]");
+    private final ElementsCollection headerLetters = $$("header");
+
+    @Test
+    public void dragAndDropShortTest() {
+        String url = "https://the-internet.herokuapp.com/drag_and_drop";
+
+        open(url);
+        columnA.dragAndDropTo(columnB);
+        columnB.shouldHave(exactText("A"));
+    }
+
+    @Test
+    public void dragAndDropLongTest() {
+        String url = "https://the-internet.herokuapp.com/drag_and_drop";
+
+        open(url);
+        actions()
+                .moveToElement(columnA,1,1)
+                .clickAndHold()
+                .moveByOffset(142,-72)
+                .release()
+                .perform();
+        columnB.shouldHave(exactText("A"));
+    }
+}
---
+++ src/test/java/github/FindJUnit5CodeTest.java
@@ -0,0 +1,34 @@
+package github;
+
+import com.codeborne.selenide.*;
+import org.junit.jupiter.api.*;
+
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selectors.*;
+import static com.codeborne.selenide.Selenide.*;
+
+public class FindJUnit5CodeTest {
+
+    @BeforeAll
+    public static void setUpBeforeAll() {
+        Configuration.headless = false;
+        Configuration.startMaximized = true;
+    }
+
+    private final SelenideElement wikiLink = $("[data-content=Wiki]");
+    private final SelenideElement showMorePagesLink = $(".wiki-more-pages-link");
+    private final SelenideElement chaptersList = $(".markdown-body");
+    private final ElementsCollection softAssertionsExamples = $$("ol[start='3']~div");
+
+    @Test
+    public void getBlockJUnit5CodeTest() {
+        String url = "https://github.com/selenide/selenide";
+        String jUnit5Code = "@ExtendWith({SoftAssertsExtension.class})";
+
+        open(url);
+        wikiLink.click();
+        showMorePagesLink.click();
+        chaptersList.$(byText("Soft assertions")).click();
+        softAssertionsExamples.first().shouldHave(text(jUnit5Code));
+    }
+}
